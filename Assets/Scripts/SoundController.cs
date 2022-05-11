2824bb5aea47b58d473e932b4c57bacecf05601f
A1exdV <alexdvapple@gmail.com>
2022-05-12 02:11:42 +0300

    Added sounds

---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SoundController : MonoBehaviour
+{
+    [SerializeField] private AudioClip jump;
+
+    [SerializeField] private AudioClip ballBounce;
+
+    [SerializeField] private List<AudioClip> steps;
+    [SerializeField] private AudioSource playerAudioSource;
+
+    private BallController _ballController;
+
+    public void Start()
+    {
+        _ballController = GetComponent<BallController>();
+    }
+
+    public void Step()
+    {
+        playerAudioSource.clip = steps[Random.Range(0, steps.Count - 1)];
+        playerAudioSource.Play();
+    }
+
+    public void Bounce()
+    {
+        if (_ballController._ballInHand)
+        {
+            playerAudioSource.clip = ballBounce;
+            playerAudioSource.Play();
+        }
+    }
+
+    public void Jump()
+    {
+        playerAudioSource.clip = jump;
+        playerAudioSource.Play();
+    }
+}
