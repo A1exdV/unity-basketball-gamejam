ee188a5c3ae68db4852aafb945c608d0d980b92b
A1exdV <alexdvapple@gmail.com>
2022-05-12 16:02:28 +0300

    Added Buttons and End game panel

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+   [SerializeField] private AudioListener sound;
+   [SerializeField] private TextMeshProUGUI soundText;
+
+   public void RestartButton()
+   {
+      SceneManager.LoadScene(0);
+   }
+
+   public void SoundState()
+   {
+      if (sound.enabled)
+      {
+         sound.enabled = false;
+         soundText.text = "Sound: OFF";
+      }
+      else
+      {
+         sound.enabled = true;
+         soundText.text = "Sound: ON";
+      }
+   }
+}
